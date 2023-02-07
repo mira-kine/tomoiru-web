b8e9ed172bdf13931a45223a349d66687c598f77
mira-kine <mirak0419@gmail.com>
2023-02-06 16:06:24 -0800

    working on auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import SignIn from './views/Auth/SignIn';
+import SignUp from './views/Auth/SignUp';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <SignIn />
+    </BrowserRouter>
   );
 }
 
