7f6ac8bf63f71bf31783efd8657b70477741eeae
mira-kine <mirak0419@gmail.com>
2023-02-07 19:51:17 -0800

    setting up for auth with supabase

---
+++ src/utils/PrivateRoute.jsx
@@ -0,0 +1,16 @@
+import { Redirect, Route } from 'react-router-dom';
+
+export default function ProtectedRoute({
+  currentUser,
+  path,
+  children,
+  ...rest
+}) {
+  return (
+    <Route
+      path={path}
+      {...rest}
+      render={() => (currentUser ? { ...children } : <Redirect to="/" />)}
+    />
+  );
+}
