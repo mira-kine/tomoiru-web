5c7d87472bb9f679470119067e49568b1cc5fe42
mira-kine <mirak0419@gmail.com>
2023-02-07 20:05:11 -0800

    coded get user, session, and sign up. next create form hook to use throughout app. Will use form for auth next

--- src/api/users.js
+++ src/api/users.js
@@ -1,10 +1,21 @@
 import { client } from './client';
 
 // get User
+export function getUser() {
+  return client.auth.session();
+}
 
 // get Session
+export function getSession() {
+  return client.auth.session();
+}
 
 // sign up user
+export async function signUpUser(email, password) {
+  const { user, error } = await client.auth.signUp({ email, password });
+  if (error) throw error;
+  return user;
+}
 
 // sign in user
 
