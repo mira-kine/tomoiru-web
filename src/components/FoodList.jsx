05a71243b60a534c8da7a767edf99737a4fbef46
mira-kine <mirak0419@gmail.com>
2023-02-11 21:16:23 -0800

    connecting food id to whatever user event clicks on in order to display correct information

--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../views/FoodRecs/FoodRecs';
 import '../views/FoodRecs/FoodRecs.css';
 
 export default function FoodList({ foodsList }) {
   // handleClick -> if I click on food name, display will change to name with description + button to eat it
+  const [showDisplay, setShowDisplay] = useState(false);
+  const [foodId, setFoodId] = useState({});
+
+  const handleDisplay = (item) => {
+    setFoodId(item.id);
+    setShowDisplay(!showDisplay);
+  };
+
+  console.log('foodId', foodId);
+
   return (
-    <div id="foodlist">
-      {foodsList.map((item) => {
-        return (
-          <div key={item.id}>
-            <ul id="foodlist-ul">
-              <li id="food-name">{item.name}</li>
-            </ul>
-            {/* <img src={ } /> - add image accordingly */}
-          </div>
-        );
-      })}
+    <div>
+      {showDisplay ? (
+        <div id="foodlist">
+          {foodsList.map((item) => {
+            return (
+              <div key={item.id}>
+                <button id="foodlist-div" onClick={handleDisplay}>
+                  <span id="food-name">{item.name}</span>
+                </button>
+
+                {/* <img src={ } /> - add image accordingly */}
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div>
+          {foodsList.map((item) => {
+            return (
+              <div key={item.id}>
+                <button onClick={handleDisplay}>{item.name}</button>
+                <span>{item.description}</span>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
