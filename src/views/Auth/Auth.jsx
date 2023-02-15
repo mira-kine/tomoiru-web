bfd0d0af64fe4ba3538020f4cad74eb788f1b13c
mira-kine <mirak0419@gmail.com>
2023-02-14 17:57:20 -0800

    fixed some conditional that was deleted on accident before

--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -3,12 +3,14 @@ import { useNavigate } from 'react-router-dom';
 import { signUpUser } from '../../api/users';
 import { signInUser } from '../../api/users';
 import AuthForm from '../../components/AuthForm/AuthForm';
+import { useUser } from '../../context/UserProvider';
 import './Auth.css';
 
 export default function Auth({ isSigningUp = false }) {
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const navigateTo = useNavigate();
+  const { currentUser } = useUser();
 
   const handleAuth = async (email, password) => {
     try {
@@ -27,7 +29,11 @@ export default function Auth({ isSigningUp = false }) {
         // sign in user
         await signInUser(email, password);
         await new Promise((r) => setTimeout(r, 1500));
-        navigateTo('/dashboard');
+        if (currentUser.has_tomo) {
+          navigateTo('/dashboard');
+        } else {
+          navigateTo('/welcome');
+        }
         setLoading(false);
       }
     } catch (error) {
