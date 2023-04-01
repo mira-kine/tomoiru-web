4639ac99b3aa232c78e63c1356f2a822422af87e
mira-kine <mirak0419@gmail.com>
2023-04-01 16:35:13 -0700

    fixing margins

--- src/views/FoodRecs/FoodRecs.jsx
+++ src/views/FoodRecs/FoodRecs.jsx
@@ -6,12 +6,15 @@ import { useUser } from '../../context/UserProvider';
 import { useTomo } from '../../hooks/useTomo';
 import './FoodRecs.css';
 import '../Dashboard/Dashboard.css';
+import NavBar from '../../components/NavBar/NavBar';
+import { useNavigate } from 'react-router-dom';
 
 export default function FoodRecs() {
   const { currentUser } = useUser();
   const tomo = useTomo({ currentUser });
   const [foodsList, setFoodsList] = useState([]);
   const [loading, setLoading] = useState(true);
+  const navigateTo = useNavigate();
 
   useEffect(() => {
     const fetchFoods = async () => {
@@ -22,22 +25,32 @@ export default function FoodRecs() {
     fetchFoods();
   }, []);
 
+  const handleHome = () => {
+    navigateTo('/dashboard');
+  };
+
   if (loading) {
     <h1>Loading...</h1>;
-  }
 
-  return (
-    <div class="home-container">
-      <div class="home-container-bg">
-        <div class="tomo-container">
-          <div class="tomo-div">
-            <YourTomo tomo={tomo} />
+    return (
+      <div class="home-container">
+        <NavBar />
+        <div id="dashboard-title-container">
+          <h1>What shall we do today?</h1>
+        </div>
+        <div class="home-container-bg">
+          <div class="user-container">
+            <div class="tomo-container">
+              <div class="tomo-div">
+                <YourTomo tomo={tomo} handleHome={handleHome} />
+              </div>
+            </div>
+            <div class="interactive-box">
+              <FoodList foodsList={foodsList} />
+            </div>
           </div>
         </div>
-        <div class="interactive-box">
-          <FoodList foodsList={foodsList} />
-        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
