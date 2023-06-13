1481d59c69b97ef2c27c0c0a765cd39c20fab542
mira-kine <mirak0419@gmail.com>
2023-06-12 18:07:09 -0700

    working on having typing animation for the rest of the messages + submitting name

--- src/components/Reusable/TextBox.jsx
+++ src/components/Reusable/TextBox.jsx
@@ -2,25 +2,6 @@ import React, { useReducer } from 'react';
 import './TextBox.css';
 import { useUser } from '../../context/UserProvider';
 
-const welcomeText = [
-  {
-    id: 0,
-    text: 'Hi there! My name is Tomomi. What is your name?',
-  },
-  {
-    id: 1,
-    text: `Awesome. Nice to meet you!`,
-  },
-  {
-    id: 2,
-    text: 'Welcome to my humble home.',
-  },
-  {
-    id: 3,
-    text: 'Here we can chat and learn Japanese together!',
-  },
-];
-
 const initialState = { index: 0 };
 function reducer(state, action) {
   switch (action.type) {
@@ -35,13 +16,33 @@ function reducer(state, action) {
 
 export default function TextBox({ userMode, setUserMode }) {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const { currentUser } = useUser();
+
+  const welcomeText = [
+    {
+      id: 0,
+      text: 'Hi there! My name is Tomomi. What is your name?',
+    },
+    {
+      id: 1,
+      text: `Awesome. Nice to meet you ${currentUser.name}!`,
+    },
+    {
+      id: 2,
+      text: 'Welcome to my humble home.',
+    },
+    {
+      id: 3,
+      text: 'Here we can chat and learn Japanese together!',
+    },
+  ];
   console.log('state', state);
 
   return (
     <div id="text-box-container">
       {/* display only the first index */}
       {/* find where the state matches the current index */}
-      <p>{welcomeText.at(state.index).text}</p>
+      <p className="typed">{welcomeText.at(state.index).text}</p>
       <button onClick={() => dispatch({ type: 'next' })}>Next</button>
       {/* when click, increment index and display that next */}
       {/* go until the end where you cannot increment anymore, change to navigate to wherever */}
