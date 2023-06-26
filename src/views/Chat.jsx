f011ce529d67d97415798137d046fffe0c89459d
mira-kine <mirak0419@gmail.com>
2023-06-26 11:06:26 -0700

    working on server separately - in the future let's try a monolithic approach

--- src/views/Chat.jsx
+++ src/views/Chat.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/Chat.css';
-import { openai } from '../api/openai';
 import Loading from '../components/Reusable/Loading';
 import { useForm } from '../hooks/useForm';
 
@@ -8,31 +7,6 @@ export default function Chat() {
   const [responses, setResponses] = useState([]);
   const [loading, setLoading] = useState(false);
   const { formState, handleForm } = useForm({ userInput: '' });
-
-  // useEffect(() => {
-  //   const fetchResponses = async () => {
-  //     try {
-  //       const tomomiResp = await openai.createCompletion({
-  //         model: 'text-curie-001',
-  //         prompt: 'Tomomi: Hey, how are you?\n You:',
-  //         temperature: 0.5,
-  //         max_tokens: 60,
-  //         top_p: 1,
-  //         frequency_penalty: 0.5,
-  //         presence_penalty: 0,
-  //         stop: ['You:'],
-  //       });
-  //       setResponses(tomomiResp);
-  //       setLoading(false);
-  //     } catch (error) {
-  //       throw error;
-  //     }
-  //   };
-  //   fetchResponses();
-  // }, []);
-
-  console.log('responses', responses);
-  console.log('loading', loading);
 
   return (
     <div className="chat-container">
