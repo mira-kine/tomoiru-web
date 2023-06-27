7eee15261386f426b5989024749b6fb35a0e6254
mira-kine <mirak0419@gmail.com>
2023-06-27 11:41:45 -0700

    preparing server side api endpoint in tomoiru-japan-data to fetch data for chat component next

--- src/views/Chat.jsx
+++ src/views/Chat.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/Chat.css';
 import Loading from '../components/Reusable/Loading';
 import { useForm } from '../hooks/useForm';
@@ -7,6 +8,12 @@ export default function Chat() {
   // const [responses, setResponses] = useState([]);
   const [loading, setLoading] = useState(false);
   const { formState, handleForm } = useForm({ userInput: '' });
+  const navigateTo = useNavigate();
+
+  const handleBack = () => {
+    setLoading(true);
+    navigateTo('/dashboard');
+  };
 
   return (
     <div className="chat-container">
@@ -29,6 +36,7 @@ export default function Chat() {
               />
             </div>
           </form>
+          <button onClick={() => handleBack()}>go back</button>
         </div>
       )}
     </div>
