c057f27ff83d8c693546f886d00b969112bd7d07
mira-kine <mirak0419@gmail.com>
2023-06-28 15:00:32 -0700

    fixed responsivity of dashboard. User input on chat side next

---
+++ src/api/payload.js
@@ -0,0 +1,4 @@
+import { OpenAIStream, OpenAIStreamPlayload } from '../../utils/OpenAIStream';
+// should use an edge function for endpoint
+// create endpoint here that will be a simple streaming call to chatGPT
+// and then call it as /api/payload.js from the client side to receive that data
