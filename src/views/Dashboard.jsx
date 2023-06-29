0c8a429d681918570c67cf30dbfd3401fa4ddb25
Mira Kinebuchi <90009901+mira-kine@users.noreply.github.com>
2023-06-28 17:18:05 -0700

    Chat room (#61)
    * added prompt building api calls and openai api stream organizer in utils
    
    * fixed responsivity of dashboard. User input on chat side next
    
    * Created client input form area, connect to server next
    
    * typescript config next to accommodate event parser
    
    * working up the tree to install typescript

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,28 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/Dashboard.css';
-import Spline from '@splinetool/react-spline';
-import Loading from '../components/Reusable/Loading';
+// import Loading from '../components/Reusable/Loading';
 
 export default function Dashboard() {
   // const navigateTo = useNavigate();
   // option to choose foods - button to direct to food recs
-  const [modelLoading, setModelLoading] = useState(true);
 
   return (
     <div className="home-container">
-      <div id="dashboard-title-container">
-        <h1>Home</h1>
+      <div className="dashboard-title-container">
+        <h1>Your Home</h1>
       </div>
       <div className="home-container-bg">
         <div className="tomomi-house-container">
-          {modelLoading && (
-            <>
-              <Loading />
-            </>
-          )}
-          <Spline
-            onLoad={() => setModelLoading(false)}
-            scene="https://prod.spline.design/kSovaMZ-tN4qcT9P/scene.splinecode"
+          <img
+            className="tomomi-house-img"
+            src={require('../assets/tomoiru-room.png')}
+            alt="built in spline 3D modeling of a corner of a room"
           />
         </div>
       </div>
