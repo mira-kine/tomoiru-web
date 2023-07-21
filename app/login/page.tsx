214244b782f6162ae71a543d6b2f48f33cb14646
mira-kine <mirak0419@gmail.com>
2023-07-21 15:58:38 -0700

    hide and show password functioning. Work through sign up flow next

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,15 +3,17 @@ import React, { useState } from 'react';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useRouter } from 'next/navigation';
 import type { Database } from '../types/supabase';
+import Image from 'next/legacy/image';
 
 // Client Components can be used to trigger the authentication process from event handlers.
 // ... aka createClientComponentClient from auth-helpers-nextjs
-import Image from 'next/legacy/image';
 
 export default function LogIn() {
   const [errorMessage, setErrorMessage] = useState('');
   const [email, setEmail] = useState('');
+  // set out password component later on in components folder
   const [password, setPassword] = useState('');
+  const [visible, setVisible] = useState<boolean>(false);
   const [view, setView] = useState('signin');
   const router = useRouter();
   const supabase = createClientComponentClient<Database>();
@@ -62,7 +64,7 @@ export default function LogIn() {
     }
   };
 
-  const handleSwitchView = () => {
+  const handleSetView = () => {
     setErrorMessage('');
     setView('signin');
   };
@@ -71,6 +73,40 @@ export default function LogIn() {
     <>
       <div className="flex relative items-center align-center justify-center h-full w-full">
         <div className="absolute inset-0">
+          {errorMessage && (
+            <div
+              className="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
+              role="alert"
+            >
+              <div className="flex">
+                <div className="py-1">
+                  <svg
+                    className="fill-current h-6 w-6 text-teal-500 mr-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+                  </svg>
+                </div>
+                <div>
+                  <p className="font-bold">Our privacy policy has changed</p>
+                  <p className="text-sm">{errorMessage}</p>
+                </div>
+              </div>
+            </div>
+          )}
+          {view === 'check-email' ? (
+            <>
+              <div
+                className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3"
+                role="alert"
+              >
+                <p className="font-bold">Please check your email to confirm</p>
+                <p className="text-sm">then sign back in to meet Tomomi!</p>
+              </div>
+            </>
+          ) : null}
+
           <Image
             src="/assets/auth_background.jpg"
             alt="drawn background of the sky"
@@ -80,8 +116,8 @@ export default function LogIn() {
         </div>
         <div className="flex flex-col items-center justify-center m-2 h-3/4 w-3/4 z-2">
           <div className="flex flex-col align-center justify-center wrap m-2 h-5/6 w-5/6">
-            <div className="flex flex-col justify-between wrap align-center mt-1 p-2 bg-melon rounded-lg opacity-80 p-4 items-center">
-              <div className="flex flex-col justify-center p-2">
+            <div className="flex flex-col justify-between wrap align-center mt-1 p-2 bg-melon rounded-lg opacity-80 p-4 items-center w-full">
+              <div className="flex flex-col justify-center p-2 items-center">
                 {view === 'signin' ? (
                   <>
                     <span className="text-4xl tablet:text-6xl laptop:text-8xl p-2 font-script flex">
@@ -104,42 +140,90 @@ export default function LogIn() {
               </div>
               {/* add check email view for now because current ver pkce + supabase does not auto confirm */}
               {view === 'check-email' ? (
-                <>
-                  <span>
-                    Check <span>{email}</span> to continue signing up
-                  </span>
-                </>
+                <></>
               ) : (
                 <>
                   <form
                     onSubmit={(e) => handleSignIn(e)}
                     className="flex flex-col m-2 justify-center content-center wrap items-center w-5/6"
                   >
-                    <input
-                      type="email"
-                      name="email"
-                      aria-label="Email"
-                      value={email}
-                      onChange={(e) => {
-                        setEmail(e.target.value);
-                      }}
-                      placeholder="Email"
-                      className="p-2 m-2 w-3/4 text-md tablet:text-xl"
-                    />
-                    <input
-                      type="password"
-                      name="password"
-                      aria-label="Password"
-                      placeholder="Password"
-                      value={password}
-                      onChange={(e) => {
-                        setPassword(e.target.value);
-                      }}
-                      minLength="8"
-                      className="p-2 m-2 w-3/4 text-md tablet:text-xl"
-                    />
-                    <div className="p-3">
-                      {errorMessage && <p>{errorMessage}</p>}
+                    <div className="m-2 w-5/6 text-md tablet:text-xl">
+                      <input
+                        value={email}
+                        type="email"
+                        name="email"
+                        aria-label="Email"
+                        onChange={(e) => {
+                          setEmail(e.target.value);
+                        }}
+                        placeholder="Email"
+                        className="p-2 m-2 w-full text-md tablet:text-xl"
+                      />
+                    </div>
+                    <div className="m-2 w-full text-md tablet:text-xl flex items-center justify-center">
+                      <input
+                        value={password}
+                        type={visible ? 'text' : 'password'}
+                        name="password"
+                        aria-label="Password"
+                        placeholder="Password"
+                        onChange={(e) => {
+                          setPassword(e.target.value);
+                        }}
+                        className="p-2 m-2 w-3/4 text-md tablet:text-xl"
+                      />
+                      {visible ? (
+                        <span
+                          className="hover:text-white p-3 hover:cursor-pointer"
+                          onClick={() => {
+                            setVisible(!visible);
+                          }}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                            />
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                            />
+                          </svg>
+                        </span>
+                      ) : (
+                        <span
+                          className="hover:text-white p-3 hover:cursor-pointer"
+                          onClick={() => {
+                            setVisible(!visible);
+                          }}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                            />
+                          </svg>
+                        </span>
+                      )}
+                    </div>
+                    <div className="p-3 flex flex-col">
                       {view === 'signin' && (
                         <>
                           <button
@@ -149,15 +233,19 @@ export default function LogIn() {
                           >
                             Sign In
                           </button>
-                          <span>Don&apos;t have an account?</span>
-                          <button
-                            onClick={() => {
-                              setView('signup');
-                            }}
-                            className="text-bold"
-                          >
-                            Sign Up
-                          </button>
+                          <div className="flex">
+                            <span className="p-1">
+                              Don&apos;t have an account?
+                            </span>
+                            <button
+                              onClick={() => {
+                                setView('signup');
+                              }}
+                              className="underline hover:text-white p-1"
+                            >
+                              Sign Up
+                            </button>
+                          </div>
                         </>
                       )}
                       {view === 'signup' && (
@@ -169,14 +257,19 @@ export default function LogIn() {
                           >
                             Sign Up
                           </button>
-                          <span>Already have an account?</span>
-                          <button
-                            onClick={() => {
-                              handleSwitchView();
-                            }}
-                          >
-                            Sign In
-                          </button>
+                          <div className="flex">
+                            <span className="p-1">
+                              Already have an account?
+                            </span>
+                            <button
+                              onClick={() => {
+                                handleSetView();
+                              }}
+                              className="underline hover:text-white p-1"
+                            >
+                              Sign In
+                            </button>
+                          </div>
                         </>
                       )}
                     </div>
