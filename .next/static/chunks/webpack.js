58b87c1b6392abc67848fb80b529a609d5ee3549
mira-kine <mirak0419@gmail.com>
2023-07-21 16:22:32 -0700

    readjusting login form ui

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a92f14b91fef5f92"; }
+/******/ 		__webpack_require__.h = function() { return "f5c831680e394f3c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
