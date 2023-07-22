ce3a7d99ab4ed8aa650e2da571f5fa4a297d5be8
mira-kine <mirak0419@gmail.com>
2023-07-21 17:57:55 -0700

    fixing types

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "37f9459b21c2c444"; }
+/******/ 		__webpack_require__.h = function() { return "03c5c0a450c4f3db"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
