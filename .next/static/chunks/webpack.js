6852ce0656bee5b736592cd97a1348f875090fc8
mira-kine <mirak0419@gmail.com>
2023-07-24 11:46:25 -0700

    updated to account for data possibly being null

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bba747f0a6710107"; }
+/******/ 		__webpack_require__.h = function() { return "86258027599052b6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
