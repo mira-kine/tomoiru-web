348c1ba3b419f6eee16c0a64326005b450e5c22b
mira-kine <mirak0419@gmail.com>
2023-07-25 14:21:22 -0700

    updated callback Route

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -3,12 +3,12 @@ import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export async function GET(req: NextRequest) {
-  const supabase = createRouteHandlerClient({ cookies });
-  const { searchParams } = new URL(req.url);
-  const code = searchParams.get('code');
+export async function GET(request: NextRequest) {
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get('code');
 
   if (code) {
+    const supabase = createRouteHandlerClient({ cookies });
     await supabase.auth.exchangeCodeForSession(code);
   }
   return NextResponse.redirect(requestUrl.origin);
