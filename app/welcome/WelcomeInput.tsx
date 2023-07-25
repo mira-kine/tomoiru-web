f74470120fadca41670edf9d682f2485fe69a24f
mira-kine <mirak0419@gmail.com>
2023-07-25 14:31:00 -0700

    updated promise type to binding handleWelcome

--- app/welcome/WelcomeInput.tsx
+++ app/welcome/WelcomeInput.tsx
@@ -11,7 +11,7 @@ export default function WelcomeInput({ session }: { session: Session | null }) {
   const supabase = createPagesBrowserClient<Database>();
 
   // updating username and send to db
-  const handleWelcome = async (username: string) => {
+  const handleWelcome = async (username: string): Promise<void> => {
     try {
       const {
         data: { user }
