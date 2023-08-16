c2ba3fc382060e07f41b5889c726db64e23929d7
mira-kine <mirak0419@gmail.com>
2023-08-16 16:21:32 -0700

    changed navbar design to daisyUI. change chat bubble to daisyUI as well

--- app/food/page.tsx
+++ app/food/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function FoodSelect() {
-  return <div>food select</div>;
+  return <div className="flex bg-chick w-full h-full justify-center items-center">food select</div>;
 }
