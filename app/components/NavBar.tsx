a425693ee001dd077fff494b01ec660479608da1
Mira Kinebuchi <90009901+mira-kine@users.noreply.github.com>
2023-08-24 22:46:28 -0700

    Dev (#96)
    Updated with responsive nav, new dashboard UI and foodlist page started

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -7,19 +7,15 @@ import Link from "next/link";
 export default function NavBar() {
   const supabase = createClientComponentClient();
   const router = useRouter();
-  // const [showNav, setShowNav] = useState(false);
   const [user, setUser] = useState({id: ''});
+  const [showNav, setShowNav] = useState(false);
 
   const handleSignOut = async () => {
     await supabase.auth.signOut();
     router.push("/login");
     router.refresh();
   };
-
-  // const handleNav = () => {
-  //   setShowNav(!showNav);
-  // };
-
+  
   useEffect(() => {
     const fetchUser = async () => {
       const {
@@ -38,18 +34,20 @@ export default function NavBar() {
   return (
     <>
       {user?.id ? (
-        <div className="w-5/6 bg-white/70 z-50 md:hidden">
-          <div className="navbar bg-white">
-            <div className="navbar">
-              <div className="dropdown">
-                <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <div className="w-5/12 z-20 flex ml-12 fixed">
+          <div className="flex items-center p-4 justify-center z-30 mt-4">
+            <div className="flex flex-col laptop:flex-row items-center cursor-point w-full">
+                <button className="bg-white/50 p-6 rounded-full hover:bg-white cursor-point" onClick={() => {setShowNav(!showNav)}}>
+                <label tabIndex={0} className="cursor-point">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 cursor-point" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </label>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-white rounded-box w-52">
+                </button>
+                {showNav ? (
+                <ul tabIndex={0} className="ml-8 p-2 mt-8 laptop:mt-0 shadow-lg bg-white rounded-box w-full flex flex-col laptop:flex-row">
                   <li>
                     <Link
                         href="/dashboard"
-                        className="block py-2 pl-3 pr-4 text-licorice rounded md:hover:bg-transparent md:border-0 md:p-0 hover:bg-peach hover:text-white md:hover:bg-transparen"
+                        className="block py-2 pl-3 pr-4 text-licorice font-sans rounded md:hover:bg-transparent md:border-0 md:p-0 hover:bg-peach hover:text-white md:hover:bg-transparen"
                         aria-current="page"
                       >
                         Home
@@ -65,7 +63,7 @@ export default function NavBar() {
                     <li>
                       <Link
                         href="/chat"
-                        className="block py-2 pl-3 pr-4 text-licorice rounded hover:bg-gray-100 md:hover:bg-trant md:border-0 md:p-0 hover:bg-peach hover:text-white md:hover:bg-transparen"
+                        className="block py-2 pl-3 pr-4 text-licorice font-sans rounded hover:bg-gray-100 md:hover:bg-trant md:border-0 md:p-0 hover:bg-peach hover:text-white md:hover:bg-transparen"
                       >
                         Chat
                       </Link>
@@ -73,7 +71,7 @@ export default function NavBar() {
                     <li>
                       <Link
                         href="/food"
-                        className="block py-2 pl-3 pr-4 text-licorice rounded hover:bg-gray-100 md:hover:bg-trant md:border-0 md:p-0 hover:bg-peach hover:text-white"
+                        className="block py-2 pl-3 pr-4 text-licorice font-sans rounded hover:bg-gray-100 md:hover:bg-trant md:border-0 md:p-0 hover:bg-peach hover:text-white"
                       >
                         Food
                       </Link>
@@ -81,7 +79,7 @@ export default function NavBar() {
                     <li>
                       <button
                         onClick={handleSignOut}
-                        className="block py-2 pl-3 pr-4 text-licorice rounded hover:bg-gray-100 md:hover:bg-trant md:border-0 md:p-0 hover:bg-peach hover:text-white md:hover:bg-transparen"
+                        className="block py-2 pl-3 pr-4 text-licorice font-sans rounded hover:bg-gray-100 md:hover:bg-trant md:border-0 md:p-0 hover:bg-peach hover:text-white md:hover:bg-transparen"
                       >
                         Sign out
                       </button>
@@ -89,35 +87,16 @@ export default function NavBar() {
                     <li>
                       <Link
                         href="/about"
-                        className="block py-2 pl-3 pr-4 text-licorice rounded hover:bg-gray-100 md:hover:bg-trant md:border-0 md:p-0 hover:bg-peach hover:text-white md:hover:bg-transparent"
+                        className="block py-2 pl-3 pr-4 text-licorice font-sans rounded hover:bg-gray-100 md:hover:bg-trant md:border-0 md:p-0 hover:bg-peach hover:text-white md:hover:bg-transparent"
                       >
                         About
                       </Link>
                     </li>
                   </ul>
+                ) : null}
                 </div>
-                <a className="btn btn-ghost normal-case text-2xl">Tomoiru</a>
               </div>
-              <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                  <li><a>Item 1</a></li>
-                  <li tabIndex={0}>
-                    <details>
-                      <summary>Parent</summary>
-                      <ul className="p-2">
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
-                      </ul>
-                    </details>
-                  </li>
-                  <li><a>Item 3</a></li>
-                </ul>
-              </div>
-              {/* <div className="navbar-end">
-                <a className="btn">Button</a>
-              </div> */}
             </div>
-        </div>
       ) : null}
     </>
   );
--- app/dashboard/dashboard.tsx
+++ app/dashboard/dashboard.tsx
@@ -2,28 +2,34 @@
 import React from 'react'
 import Image from "next/image";
 import dashboardImg from '../../public/assets/dashboard.png'
-// import { useRouter } from 'next/navigation';
+import AudioPlayer from "./audio";
+import { useRouter } from 'next/navigation';
 
 export default function DashboardClient() {
-    // const router = useRouter();
-
+    const router = useRouter();
+  
+    const handleNavChat = () => {
+      router.push('/chat')
+    }
   return (
     <>
-    <div className="absolute inset-0">
-    <Image
-        src={dashboardImg}
-        alt="drawn background of a japanese style living room"
-        sizes="(max-width: 1096px) 100vw"
-        objectFit="cover"
-        className="w-full h-full inset-0 absolute -z-1"
-        priority={true}
-      />
-    </div>
-    {/* <div className="absolute h-12 w-24 bottom-48 right-44 z-20">
-        <button onClick={() => {handleChat()}}>
-            Chat
-        </button>
-    </div> */}
-        </>
+          <div className="absolute inset-0 z-0">
+          <Image
+              src={dashboardImg}
+              alt="drawn background of a japanese style living room"
+              layout="fill"
+              className="inset-0 object-cover absolute"
+              priority={true}
+            />
+          </div>
+          <div className="bg-melon w-1/12 flex justify-center z-20 absolute right-0 top-0 mr-12 mt-4">
+            <AudioPlayer />
+          </div>
+          <div className="bg-white/50 rounded-full z-20 p-4 absolute bottom-52 right-48 hover:animate-bounce cursor-pointer">
+            <div className="bg-white p-4 rounded-full">
+              <span className="inline-block p-2 font-sans" onClick={() => {handleNavChat()}}>Chat</span>
+            </div>
+          </div>
+    </>
   )
 }
