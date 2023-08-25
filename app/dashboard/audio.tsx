8ab92e4127b3c0e6061c37f223c6ccc757bc144c
mira-kine <mirak0419@gmail.com>
2023-08-24 21:57:55 -0700

    commented out code to use for later

--- app/dashboard/audio.tsx
+++ app/dashboard/audio.tsx
@@ -1,9 +1,9 @@
 'use client'
 import React, {useState, useEffect} from 'react'
-import useSound from 'use-sound'
-import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
-import { BiSkipNext, BiSkipPrevious } from "react-icons/bi";
-import { IconContext } from "react-icons";
+// import useSound from 'use-sound'
+// import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
+// import { BiSkipNext, BiSkipPrevious } from "react-icons/bi";
+// import { IconContext } from "react-icons";
 import type { Database } from "../../types/supabase";
 import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
 
@@ -11,12 +11,12 @@ import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
 export default function AudioPlayer() {
 const supabase = createPagesBrowserClient<Database>();
 const [track, setTrack] = useState('');
-const [isPlaying, setIsPlaying] = useState(false);
-const [currTime, setCurrTime] = useState({
-    min: '',
-    sec: ''
-});
-const [seconds, setSeconds] = useState();
+// const [isPlaying, setIsPlaying] = useState(false);
+// const [currTime, setCurrTime] = useState({
+//     min: '',
+//     sec: ''
+// });
+// const [seconds, setSeconds] = useState();
 
 useEffect(() => {
     const fetchTracks = async () => {
@@ -30,7 +30,7 @@ useEffect(() => {
     })
 }, [supabase.storage]);
 
-const [play, { pause, duration, sound }] = useSound(track);
+// const [play, { pause, duration, sound }] = useSound(track);
 // useEffect(() => {
 //     const interval = setInterval(() => {
 //       if (sound) {
@@ -48,15 +48,15 @@ const [play, { pause, duration, sound }] = useSound(track);
 
 // })
 
-const playButtons = () => {
-    if (isPlaying) {
-        pause();
-        setIsPlaying(false);
-    } else {
-        play();
-        setIsPlaying(true);
-    }
-}
+// const playButtons = () => {
+//     if (isPlaying) {
+//         pause();
+//         setIsPlaying(false);
+//     } else {
+//         play();
+//         setIsPlaying(true);
+//     }
+// }
 
   return (
     <div className="w-5/6 h-1/2 flex bg-chick justify-center items-center">
