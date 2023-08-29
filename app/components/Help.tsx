73c48db9c9e70a437d00cfa3b2db8d5598c110c2
mira-kine <mirak0419@gmail.com>
2023-08-29 15:45:55 -0700

    added help button. Add help instructions next

---
+++ app/components/Help.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Help() {
+  return (
+    <>
+    <button className="btn bg-white/70">
+      Help
+    </button>
+    </>
+  )
+}
--- app/dashboard/dashboard.tsx
+++ app/dashboard/dashboard.tsx
@@ -2,17 +2,19 @@
 import React from 'react'
 import AudioPlayer from "../components/Audio/AudioPlayer";
 import DashboardCarousel from '../components/DashboardCarousel';
+import Help from '../components/Help';
 
 export default function DashboardClient() {
   return (
     <>
-          {/* <div className="absolute inset-0 z-0"> */}
           <div className="w-11/12 max-w-6xl rounded-2xl p-4 z-20 flex justify-center">
           <DashboardCarousel />
           </div>
-          {/* </div> */}
           <div className="w-3/12 flex justify-center z-50 absolute left-0 bottom-0 ml-28 mb-12">
             <AudioPlayer />
+          </div>
+          <div className="w-3/12 flex justify-center z-50 absolute right-0 bottom-0 mr-4 mb-12">
+            <Help />
           </div>
     </>
   )
