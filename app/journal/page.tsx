ed397613a0748e3cb5aba92fba713a5895a7f6d7
mira-kine <mirak0419@gmail.com>
2023-09-06 11:57:13 -0700

    added all backgrounds, structure journal next

--- app/journal/page.tsx
+++ app/journal/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/legacy/image';
+import journalbg from '../../public/assets/journal-bg.png'
 
 export default function Journal() {
   return <div className="flex w-full h-full justify-center items-center">
       <div className="absolute inset-0">
         <Image
-            src="/assets/food-bg.png"
+            src={journalbg}
             alt="drawn background of the sky"
             layout="fill"
             className="w-full h-full inset-0 object-cover absolute -z-1"
