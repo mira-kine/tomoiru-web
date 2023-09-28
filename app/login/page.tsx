e92efa49cb8acc00061ec231bc8164ad67a80fc4
mira-kine <mirak0419@gmail.com>
2023-09-27 22:28:42 -0700

    deleted duplicate import

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/navigation";
 import type { Database } from "../../types/supabase";
 import Image from "next/legacy/image";
