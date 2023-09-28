542a34166c2eedaeaf545aa48749ef5d7370f7e2
mira-kine <mirak0419@gmail.com>
2023-09-27 22:12:20 -0700

    updated food diary adding

--- app/food/foodrecs/displayinfo.tsx
+++ app/food/foodrecs/displayinfo.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
 import type { Database } from "../../../types/supabase";
 import type { Food } from '@/app/hooks/useFood';
+import { useUser } from '@/app/context/UserContextProvider';
+import toast from 'react-hot-toast'
+
 
 interface DisplayFoodInfoProps {
   foodById: Food;
@@ -12,13 +15,15 @@ interface DisplayFoodInfoProps {
 
 export default function DisplayInfo({showFood, foodById}: DisplayFoodInfoProps){
   const supabase = createPagesBrowserClient<Database>();
-
-  console.log('foodById', foodById)
+  const {user} = useUser();
   // display default page first
-  const handleAdd = async () => {
+  const handleAdd = async ({id, name, image, description}: Food) => {
     // add food by Id to my food list on supabase my_foods list
     try {
-      await supabase.from('my_foods').insert(foodById);
+      await supabase.from('my_foods').insert({
+        food_id: id, name, image, description, uuid: user.id, 
+      })
+      toast.success('added to food diary!')
     } catch (error) {
       console.error('Error upserting data', error)
     }
@@ -28,17 +33,20 @@ export default function DisplayInfo({showFood, foodById}: DisplayFoodInfoProps){
   return (
     <>
       <div className="bg-white/50 rounded-2xl overflow-y-auto display flex flex-col items-center justify-center w-1/2 my-4 px-12 mr-20 ml-4">
-        {!showFood && (
-          <div className="flex justify-center bg-chick p-8">
-            Hi! Choose a recommendation to get started
+        {!showFood ? (
+          <div className="flex justify-center p-8">
+            <span className="font-script text-2xl">Hi! Choose a recommendation to get started</span>
           </div>
-        )}
-        <div className="flex justify-center items-center wrap flex-col p-8 bg-chick h-10/12">
-          <span className="font-script">{foodById.name}</span>
-          <span className="font-sans bg-melon w-full">{foodById.description}</span>
+        ) : (
+        <>
+        <div className="flex justify-center items-center wrap flex-col p-8 h-10/12">
+          <span className="font-script text-2xl">{foodById.name}</span>
+          <span className="font-sans w-full text-xl">{foodById.description}</span>
         </div>
-          <button className="btn mt-8" onClick={() => {handleAdd()}}>Add to your wishlist</button>
-      </div>
+          <button className="btn mt-8" onClick={() => handleAdd(foodById)}>Add to your wishlist</button>
+      </>
+        )}
+        </div>
     </>
   )
 }
--- app/food/foodrecs/foodlist.tsx
+++ app/food/foodrecs/foodlist.tsx
@@ -17,23 +17,16 @@ const handleChooseFood = (id: number) => {
 
   return (
     <>
-    {/* <div className="z-30 w-9/12">
-        <button className="w-1/12 bg-chick mx-8">tabs</button>
-        <button className="w-1/12 bg-chick mx-8">tabs</button>
-        <button className="w-1/12 bg-chick mx-8">tabs</button>
-    </div> */}
     <div className="z-40 flex bg-peach w-9/12 h-3/4">
     <div className="bg-white/50 rounded-2xl overflow-y-auto display flex flex-col items-center w-1/2 my-4 px-12 ml-20 mr-4">
         <span className="font-script text-5xl text-licorice py-8">Tomomi Recs</span>
     {foodList.map((food: Food) => (
-        <>
         <div key={food.id} className="z-30">
             <button onClick={() => {handleChooseFood(food.id)}} className="
             text-licorice border-2 border-white bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-sans font-bold rounded-lg text-md tablet:text-xl laptop:text-2xl desktop:text-2xl px-5 py-2.5 text-center mr-2 mb-2 shadow-lg shadow-licorice/20">
                 {food.name}
             </button>
         </div>
-        </>
     ))}
     </div>
         <DisplayInfo showFood={showFood} foodById={foodById} />
